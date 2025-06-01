ae1db7da8a6ba1d3c81fb324b90d40bad7d9b083
antonnprince <antonprince95@gmail.com>
2025-06-01 10:53:49 +0530

    cleaning wit item pipeline

--- .venv/bookscraper/bookscraper/items.py
+++ .venv/bookscraper/bookscraper/items.py
@@ -10,3 +10,13 @@ class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class BookItem(scrapy.Item):
+    'url':Scrapy.field()
+    'title': scrapy.Field()
+    'product_type': scrapy.Field()
+    'price_excluding_tax': scrapy.Field()
+    'price_including_tax': scrapy.Field()
+    'tax': scrapy.Field()
+
+        
